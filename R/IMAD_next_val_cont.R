664c1e324ef5e0c07b35982abf82bc2959e04312
Funk <funkaj@vcu.edu>
2023-11-27 17:27:25 -0500

    Added eta_j to continuous case of IMAD

--- R/IMAD_next_val_cont.R
+++ R/IMAD_next_val_cont.R
@@ -9,16 +9,18 @@
 #' @param group_a A vector containing the values of the first group
 #' @param group_b A vector containing the values of the second group
 #' @param trtlvls A vector containing the labels of each respective group
+#' @param nu The preset clinically meaningful difference to be looked for
+#' @param pinought The target allocation ratio for the better performing group
 #' @param weight The weight of Sample Size Re-Estimation and Random Adaptive Re-Allocation
 #' @param alpha The Type 1 error rate
 #' @param beta The Type 2 error rate
-#' @param nu The preset clinically meaningful difference to be looked for
 #'
 #' @return The grouping of the next value and the expected sample sizes for each group given current means and standard deviations
 #' @export
 #'
 #' @examples IMAD_next_val_cont(rnorm(20, 1, 2), rnorm(20, 0, 2), nu = 0.8, weight = 0.7)
-IMAD_next_val_cont <- function(group_a, group_b, trtlvls = c("A", "B"), nu, weight = 0.5, alpha = 0.05, beta = 0.2){
+
+IMAD_next_val_cont <- function(group_a, group_b, trtlvls = c("A", "B"), nu, pinought = 0.5, weight = 0.5, alpha = 0.05, beta = 0.2){
   #Standard Deviation Calculation
   #Alpha deviation;
   zalpha <- qnorm(alpha/2)
@@ -35,9 +37,6 @@ IMAD_next_val_cont <- function(group_a, group_b, trtlvls = c("A", "B"), nu, weig
   n_b <- length(group_b)
   n <- n_a + n_b
 
-  #n1 <- n + 1
-  n1 <- 1
-
   #Mean of Intervention
   meana <- mean(group_a)
   #Mean of Control
@@ -48,10 +47,25 @@ IMAD_next_val_cont <- function(group_a, group_b, trtlvls = c("A", "B"), nu, weig
   #SD of Control
   sdb <- sd(group_b)
 
+  #Set optimization function for each iteration;
+  optfunc <- function(nu, sda, sdb, pinought, par){
+    faz <- (-nu) / (par * sqrt(sda^2 + sdb^2))
+    fbz <- (nu) / (par * sqrt(sda^2 + sdb^2))
+    (pinought - (sda * sqrt(pnorm(fbz)))/ (sda * sqrt(pnorm(fbz)) + sdb * sqrt(pnorm(faz))))^2
+  }
+
+  #Get optimization for given pi_nought;
+  optval <- optim(par = 1, optfunc, nu = nu, sda = sda,
+                  sdb = sdb, pinought = pinought, method = "Brent",
+                  lower = -10000, upper = 10000)
+
+  #Get eta_j for each iteration;
+  eta_j <- optval$par
+
   #Get estimated functions for a and b;
   #Identify z-scores for each variable;
-  faz <- (meana - meanb) / (n1 * sqrt(sda^2 + sdb^2))
-  fab <- (meanb - meana) / (n1 * sqrt(sda^2 + sdb^2))
+  faz <- (meana - meanb) / (eta_j * sqrt(sda^2 + sdb^2))
+  fab <- (meanb - meana) / (eta_j * sqrt(sda^2 + sdb^2))
 
   #Get normal CDF scores for a and b;
   fa <- pnorm(faz)
